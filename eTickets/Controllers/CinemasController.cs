e8cbe79d833433623bb4bf9a03ff1b700f8e6e67
Troy Szatkowski <troy.m.szatkowski@gmail.com>
2021-12-24 10:38:31 -0500

    Add cinema data, views

--- eTickets/Controllers/CinemasController.cs
+++ eTickets/Controllers/CinemasController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using eTickets.Data.Services;
+using eTickets.Models;
 
 namespace eTickets.Controllers
 {
@@ -22,6 +23,64 @@ namespace eTickets.Controllers
 		{
 			var cinemas = await _service.GetAllAsync();
 			return View(cinemas);
+		}
+
+		// GET: Cinemas/Create
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("Logo, Name, Description")] Cinema cinema)
+		{
+			if (!ModelState.IsValid) return View(cinema);
+			await _service.AddAsync(cinema);
+			return RedirectToAction(nameof(Index));
+		}
+
+		// GET: Cinemas/Details/1
+		public async Task<IActionResult> Details(int id)
+		{
+			var cinemaDetails = await _service.GetByIdAsync(id);
+			if (cinemaDetails == null) return View("NotFound");
+			return View(cinemaDetails);
+		}
+
+		// GET: Cinemas/Edit/1
+		public async Task<IActionResult> Edit(int id)
+		{
+			var cinemaDetails = await _service.GetByIdAsync(id);
+			if (cinemaDetails == null) return View("NotFound");
+			return View(cinemaDetails);
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id, [Bind("Id, Logo, Name, Description")] Cinema cinema)
+		{
+			if (!ModelState.IsValid) return View(cinema);
+			await _service.UpdateAsync(id, cinema);
+			return RedirectToAction(nameof(Index));
+		}
+
+		// GET: Cinemas/Delete/1
+		public async Task<IActionResult> Delete(int id)
+		{
+			var cinemaDetails = await _service.GetByIdAsync(id);
+			if (cinemaDetails == null) return View("NotFound");
+			return View(cinemaDetails);
+		}
+
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirm(int id)
+		{
+			var cinemaDetails = await _service.GetByIdAsync(id);
+			if (cinemaDetails == null) return View("NotFound");
+			await _service.DeleteAsync(id);
+			return RedirectToAction(nameof(Index));
 		}
 	}
 }
--- eTickets/Models/Cinema.cs
+++ eTickets/Models/Cinema.cs
@@ -12,12 +12,15 @@ namespace eTickets.Models
 		[Key]
 		public int Id { get; set; }
 
+		[Required(ErrorMessage = "Cinema logo is required")]
 		[Display(Name = "Logo")]
 		public string Logo { get; set; }
 
+		[Required(ErrorMessage = "Cinema name is required")]
 		[Display(Name = "Name")]
 		public string Name { get; set; }
 
+		[Required(ErrorMessage = "Cinema description is required")]
 		[Display(Name = "Description")]
 		public string Description { get; set; }
 
